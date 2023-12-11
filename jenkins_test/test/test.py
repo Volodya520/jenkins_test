1eef3da14c5cd6abd345e879e7927d79fcd8a616
vova <1921507475@qq.ocm>
2023-12-11 17:12:24 +0800

    修改驱动路径

--- jenkins_test/test/test.py
+++ jenkins_test/test/test.py
@@ -1,11 +1,10 @@
 import time
 from selenium import webdriver
-
-driver_path = r'C:\Users\User\Desktop\jenkins_test\driver\chromedriver.exe'
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(executable_path=driver_path)
 
+driver_path = r'C:\Users\User\Desktop\jenkins_test\jenkins_test\driver\chromedriver.exe'
+driver = webdriver.Chrome(executable_path=driver_path)
 driver.get('https://www.ulearning.app/ulearning/index.html#/i18n')
 driver.implicitly_wait(10)
 driver.find_element(By.XPATH,"/html/body/div[1]/div/div/header/nav/div/button").click()
@@ -14,3 +13,4 @@ driver.find_element(By.ID,"userLoginName").send_keys('Auto_Manageadmin1')
 driver.find_element(By.ID,"userPassword").send_keys('wenhua123')
 time.sleep(2)
 driver.quit()
+
